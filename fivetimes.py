3a2ffd2d4596958f0dc43d1886f1e0ee70bfd0a2
jonathanbanerjee <jonathan@youd.net>
2023-06-10 15:45:12 +0100

    adding my own function

---
+++ fivetimes.py
@@ -0,0 +1,21 @@
+#For Loop version
+print('This is the for loop version')
+print (' ')
+
+print('My name is')
+for i in range (5, -1 , -1):
+    print('Jimmy Five Times ' + str(i))
+
+print (' ')
+
+
+print('And now for the while loop version')
+
+print (' ')
+
+#While Loop version
+i = 0
+while i < 5:
+     print('Jimmy Five Times ' + str(i))
+     i = i + 1
+    
---
+++ scorematch.py
@@ -0,0 +1,9 @@
+def wincalc():
+    print('How many wins do you have?')
+    wins = input()
+    print('How many losses do you have?')
+    losses = input() 
+    totalgames = int(wins) + int(losses)
+    winningpercentage = int(wins) / int(totalgames) * 100
+    print('Your winning percentage in this arena is ' + str(round(float(winningpercentage), 2)) + ' %')
+wincalc()

3b55c2c12d0bc6fcda27990461664a68098698d7
jonathanbanerjee <jonathan@youd.net>
2023-06-11 22:42:22 +0100

    Adding a try-except value error to scorematch project

---
+++ example_elif.py
@@ -0,0 +1,9 @@
+print('Please enter an input')
+spam = input()
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
+
--- scorematch.py
+++ scorematch.py
@@ -1,9 +1,15 @@
 def wincalc():
-    print('How many wins do you have?')
-    wins = input()
-    print('How many losses do you have?')
-    losses = input() 
-    totalgames = int(wins) + int(losses)
-    winningpercentage = int(wins) / int(totalgames) * 100
-    print('Your winning percentage in this arena is ' + str(round(float(winningpercentage), 2)) + ' %')
+    try:
+        print('How many wins do you have?')
+        wins = input()
+        print('How many losses do you have?')
+        losses = input()
+
+        totalgames = int(wins) + int(losses)
+        winningpercentage = int(wins) / int(totalgames) * 100
+        print('Your winning percentage in this arena is ' + str(round(float(winningpercentage), 2)) + ' %')
+    except ValueError:
+        print('You did not enter a number.')
+
 wincalc()
+

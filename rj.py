41bec579c350718c1959207b772acb5f7e405fc9
jonathanbanerjee <jonathan@youd.net>
2023-07-09 14:56:01 +0100

    adding system script

--- rj.py
+++ rj.py
@@ -1,4 +1,4 @@
-spam = """The Project Gutenberg EBook of Romeo and Juliet, by William Shakespeare
+spam = """The Project Gutenberg EBook of Romeo and Juliet, by William Shakespearec
 """
 
  print(len(spam))
